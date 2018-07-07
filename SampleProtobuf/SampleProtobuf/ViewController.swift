30408ffd9b13ff52f6bc21158840efb63ff513c6
mayumi-hokama <mayumi.hokama@gmail.com>
2018-07-07 22:33:28 +0900

    quick start

--- SampleProtobuf/SampleProtobuf/ViewController.swift
+++ SampleProtobuf/SampleProtobuf/ViewController.swift
@@ -12,7 +12,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var info = BookInfo()
+        info.id = 100
+        info.title = "test"
+        info.author = "hokama"
+        
+        // get only
+        let info2 = BookInfo.with {
+            $0.id = 1735
+            $0.title = "Even More Interesting"
+            $0.author = "Jane Q. Smith"
+        }
+        
+        do {
+            // binary protobuf形式にシリアライズ
+            let binaryData: Data = try info.serializedData()
+            print("binaryData", binaryData)
+            
+            // バイナリデータを構造体へ
+            let decodedInfo = try BookInfo(serializedData: binaryData)
+            print("decodedInfo", decodedInfo)
+            
+            // JSON形式にシリアライズ
+            let jsonData: Data = try info.jsonUTF8Data()
+            print("jsonData", jsonData)
+            
+            // JSONデータから構造体へ
+            let receivedFromJSON = try BookInfo(jsonUTF8Data: jsonData)
+            print("receivedFromJSON", receivedFromJSON)
+            
+        } catch (let e) {
+            print(e)
+        }
     }
 
     override func didReceiveMemoryWarning() {
