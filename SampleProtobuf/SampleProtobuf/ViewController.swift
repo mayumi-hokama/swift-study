367c3bab17c14b6d70287ac3ca8e5a51f333e881
mayumi-hokama <mayumi.hokama@gmail.com>
2018-07-07 18:15:48 +0900

    study protobuf

---
+++ SampleProtobuf/SampleProtobuf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleProtobuf
+//
+//  Created by 外間麻友美 on 2018/07/07.
+//  Copyright © 2018年 外間麻友美. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
